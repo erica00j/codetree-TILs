f9ee25d55254627ad7be5908474659dfb270fce1
Yuri Chung <68888172+erica00j@users.noreply.github.com>
2024-08-09 21:02:22 +0900

    Title: 바이러스 백신, Time: 172ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240809/바이러스 백신/vaccine-for-virus.py
+++ 240809/바이러스 백신/vaccine-for-virus.py
@@ -26,9 +26,12 @@ def bfs(starting_point, original_graph):
                     graph[nx][ny] = 1
                     queue.append((nx,ny,time+1))
                     max_time = max(max_time, time+1)
+    for row in graph:
+        if 0 in row:
+            return -1
     return max_time
 
-#3개의 병원 고르기
+#m개의 병원 고르기
 starting_points = []
 hospital = []
 for i in range(n):
@@ -36,22 +39,23 @@ for i in range(n):
         if graph[i][j] == 2:
             hospital.append((i,j))
 #hospital = [(0, 0), (0, 2), (2, 4), (5,2)]
-def combinations(new_arr, c):
-    if len(new_arr) == 3:
+def combinations(m, new_arr, c):
+    if len(new_arr) == m:
         return [new_arr]  # 리스트에 조합을 담아 반환
     result = []
     for i in range(c, len(hospital)):
-        result += combinations(new_arr + [hospital[i]], i + 1)
+        result += combinations(m, new_arr + [hospital[i]], i + 1)
     return result
-starting_points = combinations([], 0)
+starting_points = combinations(m, [], 0)
 #starting_points = [[(0, 0), (0, 2), (2, 4)],[(0, 0), (0, 2), (5, 2)],[(0, 0), (2, 4), (5, 2)], [(0, 2), (2, 4), (5, 2)]]
 
 min_time = float('inf')
-#3개의 병원에서 백신 퍼뜨리기
-for i in range(len(starting_points)):
-    starting_point = starting_points[i]
+# m개의 병원에서 백신 퍼뜨리기
+for starting_point in starting_points:
+    # 병원 위치 조합에 대해 bfs를 실행
     time_to_infect_all = bfs(starting_point, graph)
-    min_time = min(min_time, time_to_infect_all)
+    if time_to_infect_all != -1:
+        min_time = min(min_time, time_to_infect_all)
 if min_time == float('inf'):
     print(-1)
 else:
