dd765b1f57f84e9441c63b3d704516e130bcbb81
Yuri Chung <68888172+erica00j@users.noreply.github.com>
2024-08-12 15:38:18 +0900

    Title: 외주 수익 최대화하기, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240812/외주 수익 최대화하기/max-of-outsourcing-profit.py
+++ 240812/외주 수익 최대화하기/max-of-outsourcing-profit.py
@@ -1,17 +1,13 @@
 n = int(input())
-work = [[0]]
+work = []
 for i in range(n):
     work.append(list(map(int,input().split())))
-#work = [[0], [2, 5], [1, 4]]
 
 dp = [0]*(n+1)
 day =0
-for i in range(1,n+1):
+for i in range(n):
     t, p = work[i]
-    if day + t <= n:
-        dp[i] = max(dp[i-1],dp[i-1] + p)
-        day = day + t
-    else:
-        dp[i] = max(dp[i-1], dp[i-2] +p)
-        day = i - 1 + t
+    if i + t <= n:
+        dp[i+t] = max(dp[i+t],dp[i] + p)
+    dp[i] = max(dp[i], dp[i-1])
 print(max(dp))
