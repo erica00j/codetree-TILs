cf500b72aedb97ccc219317bdcf8b9cf008b40f0
Yuri Chung <68888172+erica00j@users.noreply.github.com>
2024-08-12 15:40:51 +0900

    Title: 외주 수익 최대화하기, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 240812/외주 수익 최대화하기/max-of-outsourcing-profit.py
+++ 240812/외주 수익 최대화하기/max-of-outsourcing-profit.py
@@ -7,7 +7,8 @@ dp = [0]*(n+1)
 day =0
 for i in range(n):
     t, p = work[i]
+    dp[i] = max(dp[i], dp[i - 1])
     if i + t <= n:
         dp[i+t] = max(dp[i+t],dp[i] + p)
-    dp[i] = max(dp[i], dp[i-1])
+
 print(max(dp))
