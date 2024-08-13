9367f63df9966163fd472952b5824f0240ef5fc5
Yuri Chung <68888172+erica00j@users.noreply.github.com>
2024-08-13 21:13:54 +0900

    Title: 연산자 배치하기, Time: 2393ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 240813/연산자 배치하기/arrange-operator.py
+++ 240813/연산자 배치하기/arrange-operator.py
@@ -2,12 +2,9 @@ n = int(input())
 number = list(map(int,input().split()))
 plus, minus, multiply = map(int,input().split())
 operator = []
-for _ in range(plus):
-    operator.append('+')
-for _ in range(minus):
-    operator.append('-')
-for _ in range(multiply):
-    operator.append('*')
+operator += ['+'] * plus
+operator += ['-'] * minus
+operator += ['*'] * multiply
 #operator = ['+', '-']
 
 #가능한 연산자 순열
@@ -27,13 +24,11 @@ backtrack_perm([])
 
 #계산하기
 goal = []
-for i in range(len(result)):
-    function = []
-    for j in range(len(number)):
-        function.append(number[j])
-        if j==len(result[i]):
-            break
-        function.append(result[i][j])
+for ops in result:
+    function = [number[0]]
+    for num, op in zip(number[1:],ops):
+        function.append(op)
+        function.append(num)
     answer = function[0]
     for k in range(1,len(function),2):
         o = function[k]
